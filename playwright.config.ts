361e7b0e8ba515f39f0caa2c3a3af65710eb1154
Rafi Shaik <rafi.tgcs219@gmail.com>
2026-09-20 12:00:10 +0530

    Intial commit - Playwright_14

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+export default defineConfig({
+  testDir: './tests',
+  timeout:30*1000,
+  fullyParallel: true,
+
+  reporter: 'html',
+ 
+  use: {
+   browserName:'chromium',
+   headless:false,
+    
+  },
+
+ 
+});
---
+++ tests/login.spec.ts
@@ -0,0 +1,16 @@
+import {expect} from '@playwright/test';
+import {test} from '../fixtures/fixtures';
+import testdata from '../test-data/testdata.json';
+
+
+test("loginTest Case", async ({page,loginPage})=>{
+
+    await page.goto("https://rahulshettyacademy.com/client");
+    
+    await loginPage.login(testdata.username,testdata.password);
+
+    await expect(page).toHaveTitle("Let's Shop");
+
+    await page.pause();
+    
+});
